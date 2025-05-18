71ab937c57272adb62e45fa90e2fd78e187cdf30
Zion <u21655325@tuks.co.za>
2025-05-18 19:10:02 +0200

    fix build

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'zionvanwyk.github.io/',
   plugins: [
     vue(),
     vueDevTools(),
+    react(),
   ],
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 })
+
+
