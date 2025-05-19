37352dd98a593f928bc63d3010c541a6679ef13d
Zion <u21655325@tuks.co.za>
2025-05-19 21:40:30 +0200

    set up primevue

--- src/main.js
+++ src/main.js
@@ -3,9 +3,21 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import 'primeicons/primeicons.css';
+import Aura from '@primevue/themes/aura'
+import { definePreset } from '@primevue/themes'
+
 
 const app = createApp(App)
 
+const MyPreset = definePreset(Aura, {})
+
+app.use(PrimeVue, {
+    theme: {
+        preset: MyPreset,
+    }
+});
 app.use(router)
 
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
